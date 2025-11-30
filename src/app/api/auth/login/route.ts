3210cec3ec03006e1720d9c76f2306d53f5da175
a6iyyu <mizukinako7@gmail.com>
2025-11-30 14:40:09 +0700

    :rocket: Ready for preview?

---
+++ src/app/api/auth/login/route.ts
@@ -0,0 +1,11 @@
+import { NextRequest, NextResponse } from "next/server";
+import { API_AUTH_LOGIN } from "@/constants/route";
+
+export async function POST(_request: NextRequest): Promise<NextResponse> {
+  try {
+    return NextResponse.json({ message: `Login route at ${API_AUTH_LOGIN}` }, { status: 200 });
+  } catch (error: unknown) {
+    console.error(`❌ Error in ${API_AUTH_LOGIN} route: ${error}`);
+    return NextResponse.json({ message: "Internal Server Error" }, { status: 500 });
+  }
+}
